c9c2620754c82ade46e6da87ed54c7cf0d58fd83
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-01-04 14:22:05 -0800

    Added for loop for pokemonList with conditionals based on pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 let pokemonList  = [
-    {name:'Butterfree', type:['bug', 'flying'], height: 1.1 + 'm'},
-    {name:'Caterpie', type:'Bug', height: 0.3 + 'm'},
-    {name: 'Jigglypuff', type:['Fairy', 'Normal'], height: 0.5 + 'm'}
+    {name:'Butterfree', type:['bug', 'flying'], height: 1.1},
+    {name:'Caterpie', type:'Bug', height: 0.3},
+    {name: 'Jigglypuff', type:['Fairy', 'Normal'], height: 0.5}
 ];
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height < 0.5){
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) - Wow! That's a tiny pokemon! <br>")
+        }
+    else if (pokemonList[i].height > 1.0){
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) - Wow! That's a big pokemon! <br>")
+        }
+    else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) <br>")
+        }
+    }
