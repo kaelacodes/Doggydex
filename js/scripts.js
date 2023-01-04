90cf11b11e105e94c079f929312c2a5fde8652f5
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-01-03 17:01:07 -0800

    Create dogList array with 3 dog objects defined within it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World!');
-let favoriteFood= 'Sushi';
-document.write(favoriteFood);
+let dogList  = [
+    {name:'Greyhound', type:'Hound', lifeExpectency: 13 + 'years'},
+    {name:'Labrador Retriever', type:'Sporting', lifeExpectency: 13 + 'years'},
+    {name: 'Great Dane', type:'Working', lifeExpectency: 10 + 'years'}
+];
