2311664c2abbc7dba966fd60219090b3c65f047b
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-01-04 14:59:21 -0800

    Added comments to CSS and JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let pokemonList  = [
     {name:'Caterpie', type:'Bug', height: 0.3},
     {name: 'Jigglypuff', type:['Fairy', 'Normal'], height: 0.5}
 ];
+//prints a list pokemon names and their respective heights from pokemonList
 for (let i=0; i < pokemonList.length; i++){
     if (pokemonList[i].height < 0.5){
         document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) - Wow! That's a tiny pokemon! <br>")
