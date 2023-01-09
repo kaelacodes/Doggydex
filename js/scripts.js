c0cbabb3264e18036675a1eaaf77dd2cc3ceace9
kaelacodes <120235236+kaelacodes@users.noreply.github.com>
2023-01-09 13:28:57 -0800

    Wrap pokemonList in IIFE with variable pokemonRepository, and update code outside of IIFE to reflect those changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,48 @@
-let pokemonList  = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     {name:'Butterfree', type:['bug', 'flying'], height: 1.1},
     {name:'Caterpie', type:'Bug', height: 0.3},
     {name: 'Jigglypuff', type:['Fairy', 'Normal'], height: 0.5}
-];
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+      }
+    
+    function getAll() {
+        return pokemonList;
+      }
+    
+    return {
+        add: add,
+        getAll: getAll
+      };
+})();
 
 //prints a list pokemon names and their respective heights from pokemonList using for() loop
 
 //for (let i=0; i < pokemonList.length; i++){
 //    if (pokemonList[i].height < 0.5){
-//        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) - Wow! That's a tiny pokemon! <br>")
+//        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow! That's a tiny pokemon! <br>")
 //        }
 //    else if (pokemonList[i].height > 1.0){
-//        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) - Wow! That's a big pokemon! <br>")
+//        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) - Wow! That's a big pokemon! <br>")
 //        }
 //    else {
-//        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +"m) <br>")
+//        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m) <br>")
 //        }
 //    }
 
-//prints a list pokemon names and their respective heights from pokemonList using forEach() loop
+//prints a list pokemon names and their respective heights from pokemonList using forEach() loop instead of for() loop
 
-pokemonList.forEach (function (pokemonList){
-    document.write (pokemonList.name + " (height: " + pokemonList.height + "m) <br>");
-});
-pokemonList.forEach(pokemonList);
+pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height < 0.5) {
+        document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow! That's a tiny pokemon! <br>")
+    } else if (pokemon.height > 1.0) {
+          document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow! That's a big pokemon! <br>")
+    } else {
+          document.write(pokemon.name + " (height: " + pokemon.height + "m) <br>")
+    }
+})
 
 
 
